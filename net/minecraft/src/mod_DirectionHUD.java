f176fc6179d67e94ba22c30878aac30b24f7b602
bspkrs <bspkrs@gmail.com>
2012-12-24 00:27:13 -0500

    1.4.6 update

--- net/minecraft/src/mod_DirectionHUD.java
+++ net/minecraft/src/mod_DirectionHUD.java
@@ -1,6 +1,9 @@
 package net.minecraft.src;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiChat;
+import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.util.MathHelper;
 
 import org.lwjgl.opengl.GL11;
 
@@ -9,40 +12,39 @@ import bspkrs.util.client.HUDUtils;
 
 public class mod_DirectionHUD extends BaseMod
 {
-    protected float                 zLevel               = 0.0F;
-    private ScaledResolution        scaledResolution;
+    protected float           zLevel               = 0.0F;
+    private ScaledResolution  scaledResolution;
     @MLProp(info = "Set to true to allow checking for mod updates, false to disable")
-    public static boolean           allowUpdateCheck     = true;
+    public static boolean     allowUpdateCheck     = true;
     @MLProp(info = "Valid alignment strings are topleft, topcenter, topright, middleleft, middlecenter, middleright, bottomleft, bottomcenter, bottomright")
-    public static String            alignMode            = "topcenter";
+    public static String      alignMode            = "topcenter";
     @MLProp(info = "Valid color values are 0-9, a-f (color values can be found here: http://www.minecraftwiki.net/wiki/File:Colors.png)")
-    public static String            markerColor          = "c";
+    public static String      markerColor          = "c";
     @MLProp(info = "The filename of the compass PNG image file. The file must be located in the /gui/ folder of the DirectionHUD zip file or minecraft.jar.")
-    public static String            imageFileName        = "compass.png";
+    public static String      imageFileName        = "compass.png";
     @MLProp(info = "Index of the selected compass in the compass image file starting at 0. Up to 10 compasses can fit in the image (10 would be index 9). Each compass is 24 pixels tall (two lines of height 12).", min = 0, max = 9)
-    public static int               compassIndex         = 0;
+    public static int         compassIndex         = 0;
     @MLProp(info = "Horizontal offset from the edge of the screen (when using right alignments the x offset is relative to the right edge of the screen)")
-    public static int               xOffset              = 2;
+    public static int         xOffset              = 2;
     @MLProp(info = "Vertical offset from the edge of the screen (when using bottom alignments the y offset is relative to the bottom edge of the screen)")
-    public static int               yOffset              = 2;
+    public static int         yOffset              = 2;
     @MLProp(info = "Vertical offset used only for the bottomcenter alignment to avoid the vanilla HUD")
-    public static int               yOffsetBottomCenter  = 41;
+    public static int         yOffsetBottomCenter  = 41;
     @MLProp(info = "Set to true if you want the xOffset value to be applied when using a center alignment")
-    public static boolean           applyXOffsetToCenter = false;
+    public static boolean     applyXOffsetToCenter = false;
     @MLProp(info = "Set to true if you want the yOffset value to be applied when using a middle alignment")
-    public static boolean           applyYOffsetToMiddle = false;
+    public static boolean     applyYOffsetToMiddle = false;
     @MLProp(info = "Set to true to show info when chat is open, false to disable info when chat is open\n\n**ONLY EDIT WHAT IS BELOW THIS**")
-    public static boolean           showInChat           = true;
+    public static boolean     showInChat           = true;
     
-    private boolean                 checkUpdate;
-    private final ModVersionChecker versionChecker;
-    private final String            versionURL           = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.5/directionHUD.version";
-    private final String            mcfTopic             = "http://www.minecraftforum.net/topic/1114612-";
+    private ModVersionChecker versionChecker;
+    private final String      versionURL           = "https://dl.dropbox.com/u/20748481/Minecraft/1.4.6/directionHUD.version";
+    private final String      mcfTopic             = "http://www.minecraftforum.net/topic/1114612-";
     
     public mod_DirectionHUD()
     {
-        versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic, ModLoader.getLogger());
-        checkUpdate = allowUpdateCheck;
+        if (allowUpdateCheck)
+            versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic, ModLoader.getLogger());
     }
     
     @Override
@@ -54,13 +56,15 @@ public class mod_DirectionHUD extends BaseMod
     @Override
     public String getVersion()
     {
-        return "v1.8(1.4.5)";
+        return "v1.8(1.4.6)";
     }
     
     @Override
     public void load()
     {
-        versionChecker.checkVersionWithLogging();
+        if (allowUpdateCheck)
+            versionChecker.checkVersionWithLogging();
+        
         ModLoader.setInGameHook(this, true, false);
     }
     
@@ -73,12 +77,12 @@ public class mod_DirectionHUD extends BaseMod
             displayHUD(mc);
         }
         
-        if (checkUpdate)
+        if (allowUpdateCheck)
         {
             if (!versionChecker.isCurrentVersion())
                 for (String msg : versionChecker.getInGameMessage())
                     mc.thePlayer.addChatMessage(msg);
-            checkUpdate = false;
+            allowUpdateCheck = false;
         }
         return true;
     }
