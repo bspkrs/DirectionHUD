4d77616c324ac1ee1d2d9c849a68b9a2fba61d22
bspkrs <bspkrs@gmail.com>
2013-04-23 00:39:43 -0400

    fixed font texture binding

--- src/net/minecraft/src/mod_DirectionHUD.java
+++ src/net/minecraft/src/mod_DirectionHUD.java
@@ -8,31 +8,33 @@ import net.minecraft.util.MathHelper;
 import org.lwjgl.opengl.GL11;
 
 import bspkrs.client.util.HUDUtils;
+import bspkrs.util.BSProp;
+import bspkrs.util.BSPropRegistry;
 import bspkrs.util.ModVersionChecker;
 
 public class mod_DirectionHUD extends BaseMod
 {
     protected float           zLevel               = 0.0F;
     private ScaledResolution  scaledResolution;
-    @MLProp(info = "Valid alignment strings are topleft, topcenter, topright, middleleft, middlecenter, middleright, bottomleft, bottomcenter, bottomright")
+    @BSProp(info = "Valid alignment strings are topleft, topcenter, topright, middleleft, middlecenter, middleright, bottomleft, bottomcenter, bottomright")
     public static String      alignMode            = "topcenter";
-    @MLProp(info = "Valid color values are 0-9, a-f (color values can be found here: http://www.minecraftwiki.net/wiki/File:Colors.png)")
+    @BSProp(info = "Valid color values are 0-9, a-f (color values can be found here: http://www.minecraftwiki.net/wiki/File:Colors.png)")
     public static String      markerColor          = "c";
-    @MLProp(info = "The filename of the compass PNG image file. The file must be located in the /gui/ folder of the DirectionHUD zip file or minecraft.jar.")
+    @BSProp(info = "The filename of the compass PNG image file. The file must be located in the /gui/ folder of the DirectionHUD zip file or minecraft.jar.")
     public static String      imageFileName        = "compass.png";
-    @MLProp(info = "Index of the selected compass in the compass image file starting at 0. Up to 10 compasses can fit in the image (10 would be index 9). Each compass is 24 pixels tall (two lines of height 12).", min = 0, max = 9)
+    @BSProp(info = "Index of the selected compass in the compass image file starting at 0. Up to 10 compasses can fit in the image (10 would be index 9). Each compass is 24 pixels tall (two lines of height 12).", min = 0, max = 9)
     public static int         compassIndex         = 0;
-    @MLProp(info = "Horizontal offset from the edge of the screen (when using right alignments the x offset is relative to the right edge of the screen)")
+    @BSProp(info = "Horizontal offset from the edge of the screen (when using right alignments the x offset is relative to the right edge of the screen)")
     public static int         xOffset              = 2;
-    @MLProp(info = "Vertical offset from the edge of the screen (when using bottom alignments the y offset is relative to the bottom edge of the screen)")
+    @BSProp(info = "Vertical offset from the edge of the screen (when using bottom alignments the y offset is relative to the bottom edge of the screen)")
     public static int         yOffset              = 2;
-    @MLProp(info = "Vertical offset used only for the bottomcenter alignment to avoid the vanilla HUD")
+    @BSProp(info = "Vertical offset used only for the bottomcenter alignment to avoid the vanilla HUD")
     public static int         yOffsetBottomCenter  = 41;
-    @MLProp(info = "Set to true if you want the xOffset value to be applied when using a center alignment")
+    @BSProp(info = "Set to true if you want the xOffset value to be applied when using a center alignment")
     public static boolean     applyXOffsetToCenter = false;
-    @MLProp(info = "Set to true if you want the yOffset value to be applied when using a middle alignment")
+    @BSProp(info = "Set to true if you want the yOffset value to be applied when using a middle alignment")
     public static boolean     applyYOffsetToMiddle = false;
-    @MLProp(info = "Set to true to show info when chat is open, false to disable info when chat is open\n\n**ONLY EDIT WHAT IS BELOW THIS**")
+    @BSProp(info = "Set to true to show info when chat is open, false to disable info when chat is open\n\n**ONLY EDIT WHAT IS BELOW THIS**")
     public static boolean     showInChat           = true;
     
     private ModVersionChecker versionChecker;
@@ -42,9 +44,7 @@ public class mod_DirectionHUD extends BaseMod
     
     public mod_DirectionHUD()
     {
-        allowUpdateCheck = mod_bspkrsCore.allowUpdateCheck;
-        if (allowUpdateCheck)
-            versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic);
+        BSPropRegistry.registerPropHandler(this.getClass());
     }
     
     @Override
@@ -56,7 +56,7 @@ public class mod_DirectionHUD extends BaseMod
     @Override
     public String getVersion()
     {
-        return "v1.10(1.5.1)";
+        return "v1.11(1.5.1)";
     }
     
     @Override
@@ -68,8 +68,12 @@ public class mod_DirectionHUD extends BaseMod
     @Override
     public void load()
     {
+        allowUpdateCheck = mod_bspkrsCore.allowUpdateCheck;
         if (allowUpdateCheck)
+        {
             versionChecker.checkVersionWithLogging();
+            versionChecker = new ModVersionChecker(getName(), getVersion(), versionURL, mcfTopic);
+        }
         
         ModLoader.setInGameHook(this, true, false);
     }
@@ -124,10 +128,13 @@ public class mod_DirectionHUD extends BaseMod
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         mc.renderEngine.bindTexture("/gui/" + imageFileName);
         int xBase = getX(65);
+        
         if (direction < 128)
             HUDUtils.drawTexturedModalRect(xBase, yBase, direction, (compassIndex * 24), 65, 12, zLevel);
         else
             HUDUtils.drawTexturedModalRect(xBase, yBase, direction - 128, (compassIndex * 24) + 12, 65, 12, zLevel);
+        
+        GL11.glBindTexture(3553, mc.renderEngine.getTexture("/font/default.png"));
         mc.fontRenderer.drawString("\247" + markerColor.toLowerCase() + "|\247r", xBase + 32, yBase + 1, 0xffffff);
         mc.fontRenderer.drawString("\247" + markerColor.toLowerCase() + "|\247r", xBase + 32, yBase + 5, 0xffffff);
     }
