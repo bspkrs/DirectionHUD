2e5f2b2bd8b95237449e055a429720bcb9f453d7
bspkrs <bspkrs@gmail.com>
2014-06-30 00:04:59 -0400

    1.7.10 update

--- src/main/java/bspkrs/directionhud/DirectionHUD.java
+++ src/main/java/bspkrs/directionhud/DirectionHUD.java
@@ -110,7 +110,7 @@ public class DirectionHUD
                 && !mc.gameSettings.showDebugInfo)
         {
             GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
-            scaledResolution = new ScaledResolution(mc.gameSettings, mc.displayWidth, mc.displayHeight);
+            scaledResolution = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
             displayHUD(mc);
             GL11.glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
         }
