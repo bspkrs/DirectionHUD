2cd74672b9ca5e61c88064471365eff592ac0819
bspkrs <bspkrs@gmail.com>
2014-08-07 17:14:52 -0400

    update README.md

--- src/main/java/bspkrs/directionhud/fml/gui/GuiDHConfig.java
+++ src/main/java/bspkrs/directionhud/fml/gui/GuiDHConfig.java
@@ -8,6 +8,7 @@ import cpw.mods.fml.client.config.GuiConfig;
 
 public class GuiDHConfig extends GuiConfig
 {
+    @SuppressWarnings("rawtypes")
     public GuiDHConfig(GuiScreen parent)
     {
         super(parent, (new ConfigElement(Reference.config.getCategory(Configuration.CATEGORY_GENERAL))).getChildElements(),
